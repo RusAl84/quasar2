72dc7c83841f97de436d8c7fdb092601ff3dbf97
Aleksey <rusal@bk.ru>
2022-03-24 13:57:02 +0300

    a

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="flex flex-center">
+
+<q-card>
+  <q-card-section>
+ <q-input outlined v-model="text" label="Outlined" />
+
+  </q-card-section>
+  <q-card-section>
+    <q-card>
+{{text}}
+    </q-card>
+  </q-card-section>
+</q-card>
+
+
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent,ref } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup(){
+    return {text:ref("")}
+  }
+})
+</script>
